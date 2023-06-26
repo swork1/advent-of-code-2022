847cc82082938d293c75550262dd0f2885c4710b
Seth Workman <Saworkman1@gmail.com>
2023-06-25 21:20:30 -0500

    switch to seed7

--- bin/day03.ml
+++
@@ -1,6 +0,0 @@
-let file = "inputs/03/input.txt"
-
-let read_lines file =
-  In_channel.open_bin file |> In_channel.input_all |> String.split_on_char '\n'
-
-let () = read_lines file |> List.iter print_endline
